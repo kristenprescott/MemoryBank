e18b57065fd01e06c8047069efbcf12809ee2425
Kristen Prescott <prescottdarling@gmail.com>
2021-06-29 17:52:08 -0400

    added Cards to Post component + like/delete/more icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,61 @@
 import React from "react";
 import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbsUpIcon from "../../../images/like.png";
+import MoreHorIcon from "../../../images/more.png";
+import DeleteIcon from "../../../images/delete.png";
+import moment from "moment";
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
 
   return (
-    <div>
-      <h1>POST</h1>
-    </div>
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <img style={{ width: "30%" }} src={MoreHorIcon} />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <CardContent>
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        {" "}
+        <Button size="small" color="primary" onClick={() => {}}>
+          <img style={{ width: "30%" }} src={DeleteIcon} />
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <img style={{ width: "30%" }} src={ThumbsUpIcon} />
+          <hr></hr>
+          {post.likeCount}
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
 
 const Posts = () => {
-  const classes = useStyles();
   const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
-  console.log("Posts: ", posts);
-
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 

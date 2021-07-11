0274da829ec7e763e408562cd680f0f28ca80aed
KayPea <prescottdarling@gmail.com>
2021-07-11 19:23:21 -0400

    updated constants/actionTypes to add COMMENT]

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,6 +4,7 @@ export const FETCH_POST = "FETCH_POST";
 export const CREATE = "CREATE";
 export const UPDATE = "UPDATE";
 export const LIKE = "LIKE";
+export const COMMENT = "COMMENT";
 export const DELETE = "DELETE";
 
 export const START_LOADING = "START_LOADING";
